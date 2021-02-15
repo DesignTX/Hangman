4d301d726af3e1afebf8c3894d4bda11332821ed
Tony Xu <Design.TXU@gmail.com>
2021-02-15 22:19:42 +1100

    created base hangman game with the word instance property as an array of lower case letters

--- hangman.js
+++ hangman.js
@@ -4,3 +4,15 @@
 
 // No guesses? -> ***
 // Guessed "c", "b", and "t"? -> c*t
+
+
+const Hangman = function (word, remainingGuesses) {
+    this.word = word
+    this.remainingGuesses = remainingGuesses
+}
+
+const game1 = new Hangman('Dog', 2)
+console.log(game1.getPuzzle())
+
+const game2 = new Hangman('Hello World', 4)
+console.log(game2.getPuzzle())
