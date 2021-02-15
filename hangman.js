7a1fa5fa294ec8cd1a221b15f57d1fea0fe9d757
Tony Xu <Design.TXU@gmail.com>
2021-02-15 22:17:22 +1100

    added hangman challenge

---
+++ hangman.js
@@ -0,0 +1,6 @@
+// 1. Set up the word instance property as an array of lower case letters
+// 2. Set up another instance property to store guessed letters
+// 3. Create a method that gives you the word puzzle back
+
+// No guesses? -> ***
+// Guessed "c", "b", and "t"? -> c*t
