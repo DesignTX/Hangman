478216cbc46b35873e41920e5766c75a6946b8bf
Tony Xu <Design.TXU@gmail.com>
2021-02-18 06:31:22 +1100

    Created app.js file, moved game details and eventlistener to app.js, added source script to index.html

--- hangman.js
+++ hangman.js
@@ -33,22 +33,3 @@ Hangman.prototype.makeGuess = function (guess) {
         this.remainingGuesses--
     }
 }
-
-const puzzleElement = document.querySelector('#puzzle')
-const guessElement = document.querySelector('#guess')
-const game1 = new Hangman('Hello World', 4)
-puzzleElement.textContent = game1.getPuzzle()
-guessElement.textContent = game1.remainingGuesses
-
-console.log(game1.getPuzzle())
-console.log(game1.remainingGuesses)
-
-//registers key presses
-window.addEventListener('keypress', function (e) {
-    //CharCode deprecated, grab key straight from property eg; e.key
-    //console.log(e.key)
-    const guess = String(e.key)
-    game1.makeGuess(guess)
-    puzzleElement.textContent = game1.getPuzzle()
-    guessElement.textContent = game1.remainingGuesses
-})
